d513b5ccb51c36dfadbd2fc855e73bebc4ad7070
Dan Tran <nmdt2@cam.ac.uk>
2024-01-19 17:36:20 +0800

    Derive response and measure latency of LLM generation

---
+++ run.py
@@ -0,0 +1,60 @@
+from argparse import ArgumentParser
+import json
+import time
+
+from openai import OpenAI
+import pandas as pd
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--endpoint", type=str, required=True)
+    parser.add_argument("--api_key", type=str, default="placeholder")
+    parser.add_argument("--model", type=str, default="placeholder")
+    parser.add_argument("--prompt_template", type=str, required=True)
+    parser.add_argument("--input_data", type=str, required=True)
+    parser.add_argument("--output_file", type=str, required=True)
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.input_data, encoding="unicode_escape")
+    with open(args.prompt_template, "r") as f:
+        prompt_template = json.load(f)
+    openai_client = OpenAI(base_url=args.endpoint, api_key=args.api_key)
+
+    results = []
+
+    for _, row in tqdm(df.iterrows()):
+        start = time.time()
+        timestamp_first_token = None
+        tokens = []
+
+        system_msg = prompt_template["system"]
+        user_msg = prompt_template["user"].format(**row.to_dict())
+        for chunk in openai_client.chat.completions.create(
+            model=args.model,
+            messages=[
+                dict(role="system", content=system_msg),
+                dict(role="user", content=user_msg),
+            ],
+            stream=True,
+        ):
+            if timestamp_first_token is None:
+                timestamp_first_token = time.time()
+            content = chunk.choices[0].delta.content
+            if content is not None:
+                tokens.append(content)
+        end = time.time()
+
+        results.append(
+            dict(
+                system_message=system_msg,
+                user_message=user_msg,
+                model=args.model,
+                response="".join(tokens),
+                time_to_first_token=timestamp_first_token - start,
+                time_to_completion=end - start,
+            )
+        )
+
+    pd.DataFrame(results).to_csv(args.output_file, index=False)
