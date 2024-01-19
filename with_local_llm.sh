553d9a55ee5a2f5871857706ea3d2abf188c63ef
Dan Tran <nmdt2@cam.ac.uk>
2024-01-19 17:47:24 +0800

    Add script to run with local llm

---
+++ with_local_llm.sh
@@ -0,0 +1,11 @@
+nohup python -u -m vllm.entrypoints.openai.api_server \
+       --host 0.0.0.0 \
+       --model mistralai/Mistral-7B-Instruct-v0.2 &
+
+sleep 60
+
+python run.py \
+    --endpoint http://0.0.0.0:8000/v1 \
+    --prompt_template data/prompt_template.json \
+    --input_data data/input_data.csv \
+    --output_file data/results.csv
